fbcfa104ac3572ea1d824a4ae40602e8938ffdb6
Rob and Bu <Rob and Bu@RobandBu-HP>
2013-09-12 23:15:42 -0700

    Moving circle classes in less to be nested -- less repetition Also clearing the old possible moves class

--- public/javascripts/views/GameView.js
+++ public/javascripts/views/GameView.js
@@ -61,7 +61,6 @@ function($, _, Backbone, bootstrap, boardTemplate) {
             if (this.yourTurn && !this.selectPieceDisallowed) {
                 $('.pieceselect').each(function() {
                     if ($(this).hasClass('pieceselect')) {
-                        //TODO edit model to remove selected option
                         $(this).removeClass('pieceselect');
                     }
                 });
@@ -69,13 +68,19 @@ function($, _, Backbone, bootstrap, boardTemplate) {
                 var pos = $(event.target).attr('data-position').split(',');
                 var x = pos[0] - 0;
                 var y = pos[1] - 0;
-                //TODO edit model to add selected option
+
                 var possibleMoves = this.board.findPossibleMoves(x, y);
                 this.highlightPossibleMoves(possibleMoves);
             }
         },
 
         highlightPossibleMoves: function(possibleMoves) {
+            $('.possibleMove').each(function() {
+                if ($(this).hasClass('possibleMove')) {
+                    $(this).removeClass('possibleMove');
+                }
+            });
+
             for (var i = 0, len = possibleMoves.length; i < len; i++) {
                 var possibleMove = possibleMoves[i];
                 $( "[data-position='" + possibleMove[0] + "," + possibleMove[1] + "']").addClass('possibleMove');
